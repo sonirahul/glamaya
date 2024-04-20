af274d5d5dab5fa2e968fca0ef00473c5e2b2347
Rahul Soni <soni_rahul@live.com>
2024-04-20 20:23:17 +0530

    adding changes for the uxblocks prompt

---
+++ glamaya-data-contracts-open-ai/src/main/java/com/glamaya/datacontracts/openai/GoaChatCategoryResponse.java
@@ -0,0 +1,28 @@
+package com.glamaya.datacontracts.openai;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.glamaya.datacontracts.woocommerce.Category;
+import jakarta.validation.Valid;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonInclude(Include.NON_EMPTY)
+@JsonPropertyOrder({"entity", "templates"})
+@Builder
+@Data
+public class GoaChatCategoryResponse {
+    @JsonProperty("entity")
+    @Field("entity")
+    private Category entity;
+
+    @JsonProperty("templates")
+    @Field("templates")
+    private @Valid List<GoaPromptTemplate> templates = new ArrayList<>();
+}
